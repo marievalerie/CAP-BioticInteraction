bc9dbb057b888d51c2dd9946f347d962b0f4817f
marievalerie <79691910+marievalerie@users.noreply.github.com>
2023-05-08 15:27:58 +0200

    Create run-snakemake.sh

---
+++ 01_QC_and_trimming/run-snakemake.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#$ -S /bin/bash
+#$ -N snakejob
+#$ -cwd
+#$ -j n
+#$ -m e
+#$ -M m.brasseur@leibniz-zfmk.de
+
+module load anaconda3/2020.02
+conda activate snakemake
+module load fastqc/0.11.9
+module load bowtie2/2.3.5.1
+
+a=1; b=$NSLOTS
+THREADS=$((b-a)) #snakemake requires 1 thread for job monitoring
+
+snakemake --cores ${THREADS} --snakefile Snakefile_QC
